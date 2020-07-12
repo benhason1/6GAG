13b652867cc87ce8fd7c3dcb0f3e479aa9ee509b
Ben <hason.ben2@gmail.com>
2020-07-12 21:18:34 +0300

    added Skeleton

---
+++ DB/Routes/posts/index.ts
@@ -0,0 +1,19 @@
+const routerExpress = require('express')
+
+class PostsRouter {
+    dataBaseWrapper: IDataBase
+    router: any;
+    constructor(dataBaseWrapper: IDataBase) {
+        this.dataBaseWrapper = dataBaseWrapper
+        this.router = routerExpress.Router();
+        this.__InitializeRouter();
+    }
+    
+    __InitializeRouter(){
+
+    }
+
+    
+}
+
+module.exports = PostsRouter
