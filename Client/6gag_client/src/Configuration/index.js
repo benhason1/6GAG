6969ff0b220997e38724e73168b0b708d6cdaac2
Ben <hason.ben2@gmail.com>
2020-07-14 19:03:07 +0300

    changed server configuration

--- Client/6gag_client/src/Configuration/index.js
+++ Client/6gag_client/src/Configuration/index.js
@@ -1,6 +1,6 @@
 const config = {
     serverPostsRoute : 'http://127.0.0.1:4000/posts',
-    serverFilesRoute: 'http://127.0.0.1:5000/files'
+    serverFilesRoute: 'http://127.0.0.1:4000/files'
 }
 
 module.exports = config
