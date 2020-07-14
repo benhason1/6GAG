49c0b7a95bcc1fb8ea6e1d5d18a56a2500d27baa
Ben <hason.ben2@gmail.com>
2020-07-14 09:16:28 +0300

    DB saves file and path of file

--- DB/app.js
+++ DB/app.js
@@ -2,11 +2,36 @@
 exports.__esModule = true;
 var express = require("express");
 var bodyParser = require("body-parser");
+var multer = require("multer");
 var Configuration_1 = require("./Configuration");
 var MemoryDataBase_1 = require("./Dal/MemoryDataBase");
 // const memoryDb = require('./Dal/MemoryDataBase')
 var posts_1 = require("./Routes/posts");
-var postsRouter = new posts_1["default"](MemoryDataBase_1["default"]);
+//multer initialize
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+        var filePath = Date.now() + '_' + file.originalname;
+        cb(null, filePath);
+    }
+});
+var fileFilter = function (req, file, cb) {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+    }
+};
+var upload = multer({
+    limits: { fileSize: 1024 * 1024 * 5 },
+    storage: storage,
+    fileFilter: fileFilter
+});
+var postsRouter = new posts_1["default"](MemoryDataBase_1["default"], upload);
 var app = express();
 app.use(bodyParser.json());
 app.use("/posts", postsRouter.router);
--- DB/app.ts
+++ DB/app.ts
@@ -2,6 +2,8 @@ import * as express from "express";
 
 import * as bodyParser from 'body-parser';
 
+import * as multer from 'multer' 
+
 import config from './Configuration'
 
 
@@ -10,7 +12,36 @@ import MemoryDB from './Dal/MemoryDataBase'
 // const memoryDb = require('./Dal/MemoryDataBase')
 import posts from './Routes/posts'
 
-const postsRouter = new posts(MemoryDB)
+
+//multer initialize
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+      console.log(file);
+      let filePath = Date.now() + '_' + file.originalname
+      cb(null, filePath);
+    }
+  });
+  const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  }
+  
+  
+  const upload = multer({
+    limits: { fileSize: 1024 * 1024 * 5 },
+    storage: storage,
+    fileFilter: fileFilter
+  });
+
+  
+const postsRouter = new posts(MemoryDB, upload)
+  
 
 
 const app = express()
