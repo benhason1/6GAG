b6e9c6202482aea8556b25ec9c20f219183a31bb
Ben <hason.ben2@gmail.com>
2020-07-14 16:35:59 +0300

    not running -> close to implement picture from db

--- Client/6gag_client/src/HomePage/Feed/index.js
+++ Client/6gag_client/src/HomePage/Feed/index.js
@@ -1,17 +1,43 @@
 import React from "react";
+import { Component } from 'react'
 import Post from './Post'
 
-function Feed(props) {
-    const { items } = props
-    return (
-        <ul>
-            {items.map(item => (
-                // need to change it to recive the file content from server
-                <Post postId={item.id} title={item.title} toolTip={item.altText} imageSource={require("C:\\ben\\MMS\\finalExercise\\6GAG\\Client\\6gag_client\\src\\PostImages\\Random.PNG")}></Post>
-            ))
-            }
-        </ul>
-    )
+export default class Feed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            base64data: null,
+            reader : new FileReader()
+        }
+    }
+
+    render() {
+        const { items } = this.props
+        var {base64data,reader} = this.state
+        return (
+
+            <ul>
+                {items.map(item => {
+                    console.log(item);
+                    fetch("http://127.0.0.1:5000/files/?path=uploads\\1594733005960_for_bear.jpg")
+                        .then((res) => res.blob())
+                        .then(image => {
+                            reader.readAsDataURL(image)
+                            reader.onloadend = function () {
+                                this.setstate({base64data:reader.result})
+                                // base64data = reader.result;
+                                console.log(base64data)
+                            }
+                        }
+                        )
+                    return (
+                        <Post postId={item.id} title={item.title} toolTip={item.altText} imageSource={`data:image/jpeg;base64,${base64data}`}></Post>
+                    )
+                }
+                )
+                }
+            </ul>
+        )
+    }
 }
 
-export default Feed;
