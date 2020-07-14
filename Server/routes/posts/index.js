8e14a5b4e30cfe5e6ee27d6be1fed56ca1ccf56c
Ben <hason.ben2@gmail.com>
2020-07-14 09:54:46 +0300

    added posts route

--- Server/routes/posts/index.js
+++ Server/routes/posts/index.js
@@ -1,24 +1,29 @@
 const express = require('express')
 let router = express.Router()
+const axios = require('axios')
+const config = require('../../Configuration')
+
 
 router.route('/:id')
-.get((req,res)=>{
-    res.send("returning the specified post")
-})
-.delete((req,res)=>{
-    res.send("deleting the specified post")
-})
-.put((req,res)=>{
-    res.send("updating post according to action in body working")
-})
+    .get((req, res) => {
+        res.send("returning the specified post")
+    })
+    .delete((req, res) => {
+        res.send("deleting the specified post")
+    })
+    .put((req, res) => {
+        res.send("updating post according to action in body working")
+    })
 
 router.route('/')
-.get((req,res)=>{
-    res.send("sending all top posts")
-})
-.post((req,res)=>{
-    res.send("adding post to db")
-})
+    .get((req, res) => {
+        axios.get(`${DBIp}/posts`, req)
+            .then((dbRes) => res.send(dbRes))
+    })
+    .post((req, res) => {
+        axios.post(`${DBIp}/posts`, req)
+            .then((dbRes) => res.send(dbRes))
+    })
 
 
 module.exports = router
