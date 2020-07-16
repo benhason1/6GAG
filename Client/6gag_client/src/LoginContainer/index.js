f8930b2ca377c205b5ccfa997c006d935315685a
Ben <hason.ben2@gmail.com>
2020-07-16 20:31:34 +0300

    if not connected redirected to login

--- Client/6gag_client/src/LoginContainer/index.js
+++ Client/6gag_client/src/LoginContainer/index.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
 import { AppBar, Toolbar, Tab, Tabs } from '@material-ui/core'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route,Redirect } from 'react-router-dom';
 
 export default class TopBar extends Component {
 
     render() {
         return <Router>
             <AppBar style={{ background: "#fff" }}>
-
+                <Redirect to="/login"/>
                 <Toolbar >
                     <div id="six-gag-logo">
                         6gag
                         </div>
 
-                    <Tabs style={{ color: "black" }}>
-                        <Tab label="login" component={Link} to="/login"  />
-                        <Tab label="signUp" component={Link} to="/signUp"  />
-
+                    <Tabs style={{ color: "black" }} initialSelected={1}>
+                        <Tab label="login" component={Link} to="/login" value={2} />
+                        <Tab label="signUp" component={Link} to="/signUp" value={1}  />
                     </Tabs>
 
                 </Toolbar>
